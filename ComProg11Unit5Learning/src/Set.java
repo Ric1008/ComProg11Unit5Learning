1bae9032111b5cf3d86047775142a7b894d7e99d
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2020-02-20 15:01:52 -0800

    stuff
    stuff

---
+++ ComProg11Unit5Learning/src/Set.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Set {
+    public class IntegerSet {
+        private ArrayList<Integer> set;
+
+        public IntegerSet() {
+            set = new ArrayList<>();
+
+        }
+
+    }
+}
